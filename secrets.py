3ec0d9281bdec623ea4b649b43eafedf41a158ce
joonyoungpark19 <54904921+joonyoungpark19@users.noreply.github.com>
2020-12-17 12:11:13 -0500

    Initial commit

---
+++ secrets.py
@@ -0,0 +1,6 @@
+# Make sure to fill in your spotify client_secret information
+# https://developer.spotify.com/console/post-playlists/
+# link to get OAuth token
+spotify_token = ""
+spotify_user_id = ""
+playlist_id = ""
